a364f8bceebaa1c36ef1f271b26cfbb9ac62b516
ndonga123 <pndonga30@gmail.com>
2026-01-27 22:45:14 +0300

    Update Project

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB connected");
+  try {
+    await mongoose.connect(process.env.MONGO_URI.replace("mongodb+srv", "mongodb"));
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("Mongo error:", err.message);
+    process.exit(1);
+  }
 };
--- backend/routes/Vehicle.route.js
+++ backend/routes/Vehicle.route.js
@@ -1,22 +1,19 @@
 const express = require("express");
 const auth = require("../auth");
-const Vehicle = require("../Vehicle"); 
+const Vehicle = require("../Vehicle");
 
 const router = express.Router();
 
-/* Get all vehicles */
 router.get("/", auth, async (req, res) => {
   const vehicles = await Vehicle.find();
   res.json(vehicles);
 });
 
-/* Add new vehicle */
 router.post("/", auth, async (req, res) => {
   const v = await Vehicle.create(req.body);
   res.json(v);
 });
 
-/* Update GPS + sensors */
 router.put("/:id/telemetry", async (req, res) => {
   const v = await Vehicle.findByIdAndUpdate(
     req.params.id,
