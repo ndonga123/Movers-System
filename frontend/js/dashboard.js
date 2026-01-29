b79a3c4e13cb87fe81e19f75ab898a4ca2297b26
ndonga123 <pndonga30@gmail.com>
2026-01-29 15:46:55 +0300

    Update

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -1,7 +1,8 @@
-if(!localStorage.getItem("auth")){
-  location.href = "login.html";
-}
+setInterval(() => {
+  const lat = (-1.2997 + Math.random() * 0.01).toFixed(5);
+  const lng = (36.8219 + Math.random() * 0.01).toFixed(5);
 
-function toggleDark(){
-  document.body.classList.toggle("dark");
-}
+  document.getElementById("gps").innerText = `${lat}, ${lng}`;
+  document.getElementById("temp").innerText = (20 + Math.random() * 10).toFixed(1) + "°C";
+  document.getElementById("hum").innerText = (50 + Math.random() * 20).toFixed(1) + "%";
+}, 2000);
