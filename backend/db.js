9bb41460f9ab483a7c92f2fe02649593cdfd3c38
ndonga123 <pndonga30@gmail.com>
2026-02-02 16:15:53 +0300

    update

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI.replace("mongodb+srv", "mongodb"));
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected");
   } catch (err) {
     console.error("Mongo error:", err.message);
